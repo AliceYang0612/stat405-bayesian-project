4aa0f2c495f384d93aa20224fa1c09c066117496
AliceYang0612 <115049211+AliceYang0612@users.noreply.github.com>
2026-03-31 09:21:56 -0700

    Change dataset and improve EDA visualizations
    Updated the dataset source and enhanced summary statistics and visualizations for bike rentals.

--- code/eda.R
+++ code/eda.R
@@ -1,11 +1,37 @@
-df <- read.csv("../data/day.csv")
+df <- read.csv("data/london_merged.csv")
 
-summary(df$cnt)
-summary(df$temp)
+str(df)
+summary(df)
 
-hist(df$cnt, main="Distribution of Bike Rentals")
+names(df)
 
-plot(df$temp, df$cnt,
-     xlab="Temperature",
-     ylab="Bike Rentals",
-     main="Temperature vs Bike Rentals")
+summary(df[, c("cnt", "t1", "hum", "wind_speed")])
+
+hist(df$cnt,
+     breaks = 40,
+     main = "Distribution of Hourly Bike Rentals",
+     xlab = "cnt")
+
+plot(df$t1, df$cnt,
+     pch = 16, cex = 0.4,
+     xlab = "Temperature (t1)",
+     ylab = "Hourly Bike Rentals",
+     main = "Temperature vs Bike Rentals")
+
+plot(df$hum, df$cnt,
+     pch = 16, cex = 0.4,
+     xlab = "Humidity",
+     ylab = "Hourly Bike Rentals",
+     main = "Humidity vs Bike Rentals")
+
+plot(df$wind_speed, df$cnt,
+     pch = 16, cex = 0.4,
+     xlab = "Wind Speed",
+     ylab = "Hourly Bike Rentals",
+     main = "Wind Speed vs Bike Rentals")
+
+boxplot(cnt ~ is_weekend,
+        data = df,
+        main = "Bike Rentals by Weekend Indicator",
+        xlab = "is_weekend",
+        ylab = "Hourly Bike Rentals")
